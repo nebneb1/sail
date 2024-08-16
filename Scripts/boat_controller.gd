ea78208bcb67d316c0ed3f61b2011ec33e292591
quis345 <mr.smiley.face515@gmail.com>
2024-08-16 14:28:38 -0700

    added temp boat and code outlines

--- Scripts/boat_controller.gd
+++ Scripts/boat_controller.gd
@@ -1,1 +1,14 @@
 extends Node3D
+
+@export var body : Node3D
+@export var main_sheet : Node3D
+@export var jib : Node3D # the smaller sail thats closer to the front
+@export var telltale : Node3D # this is the thing on top that tells the wind direction
+@export var tiller : Node3D # this is the rudder at the back that steers
+
+@export var max_speed : float = 6.17 # approx max speed for a sloop
+
+var speed := 0.0
+
+func _process(delta: float):
+	pass
---
+++ Scripts/global.gd
@@ -0,0 +1,5 @@
+extends Node
+
+var wind_direction : Vector2 = Vector2(1.0,0)
+
+
